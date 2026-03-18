30da4f29eb47078a8644a8653dc35f3d06f70279
rathodkanishk-pixel <rathod.kanishk@gmail.com>
2026-03-18 22:20:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@
 //  That's it. Save this file and your website will work.
 // ═══════════════════════════════════════════════════════════
 
-const SUPABASE_URL = https://mtoyckxekmzpalvajheh.supabase.co
-const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10b3lja3hla216cGFsdmFqaGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzY4MjgsImV4cCI6MjA4OTQxMjgyOH0.emu173blpsNV2BMWipylsuW-NxuWK77vGPQib7HZOMk
+const SUPABASE_URL = 'https://mtoyckxekmzpalvajheh.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10b3lja3hla216cGFsdmFqaGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzY4MjgsImV4cCI6MjA4OTQxMjgyOH0.emu173blpsNV2BMWipylsuW-NxuWK77vGPQib7HZOMk'
 
 // DO NOT change anything below this line
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
